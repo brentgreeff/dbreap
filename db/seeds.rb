eaae2d6f90a49671d060fdf0b1ed16452018e5fb
Brent <brentgreeff@gmail.com>
2021-06-04 21:25:17 +0200

    Use new FixtureSet not create_fixtures anymore.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
 
 require 'active_record/fixtures'
 FIXTURES_PATHS = ["db/fixtures", "db/fixtures/#{Rails.env}"]
 
 for path in FIXTURES_PATHS
-  Dir.glob("#{RAILS_ROOT}/#{path}/" + '*.yml').each do |file|
-    Fixtures.create_fixtures(path, File.basename(file, '.*'))
+  Dir.glob("#{Rails.root}/#{path}/" + '*.yml').each do |file|
+    puts File.basename(file, '.*')
+    ActiveRecord::FixtureSet.create_fixtures(path, File.basename(file, '.*'))
   end
 end
